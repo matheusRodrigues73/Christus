c444d1ebb1059214b98fd7101bc82f3fb339920b
Math_Rodrigues <matheusrodriguesm2104@gmail.com>
2025-04-12 14:13:06 -0300

    add .next to .gitignore and `pages` directory

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+export default function Home() {
+  return (
+    <>
+      <h1>Glorificat</h1>
+      <h2>Mateus 5</h2>
+      <p className="reference">
+        3 Bem-aventurados os pobres de espírito, porque deles é o Reino dos
+        céus; 4 bem-aventurados os que choram, porque eles serão consolados; 5
+        bem-aventurados os mansos, porque eles herdarão a terra; 6
+        bem-aventurados os que têm fome e sede de justiça, porque eles serão
+        fartos; 7 bem-aventurados os misericordiosos, porque eles alcançarão
+        misericórdia; 8 bem-aventurados os limpos de coração, porque eles verão
+        a Deus; 9 bem-aventurados os pacificadores, porque eles serão chamados
+        filhos de Deus; 10 bem-aventurados os que sofrem perseguição por causa
+        da justiça, porque deles é o Reino dos céus; 11 bem-aventurados sois vós
+        quando vos injuriarem, e perseguirem, e, mentindo, disserem todo o mal
+        contra vós, por minha causa. 12 Exultai e alegrai-vos, porque é grande o
+        vosso galardão nos céus; porque assim perseguiram os profetas que foram
+        antes de vós.
+      </p>
+    </>
+  );
+}
