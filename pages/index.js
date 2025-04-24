57eee5dd243675bf93afbe2b70fd5a0a12265f00
Math_Rodrigues <matheusrodriguesm2104@gmail.com>
2025-04-24 19:39:29 -0300

    install `tailwindcss` and write home page(incomplete)

--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,134 @@ export default function Home() {
   return (
     <>
       <h1>Glorificat</h1>
-      <h2>Mateus 5</h2>
-      <p className="reference">
-        3 Bem-aventurados os pobres de espírito, porque deles é o Reino dos
-        céus; 4 bem-aventurados os que choram, porque eles serão consolados; 5
-        bem-aventurados os mansos, porque eles herdarão a terra; 6
-        bem-aventurados os que têm fome e sede de justiça, porque eles serão
-        fartos; 7 bem-aventurados os misericordiosos, porque eles alcançarão
-        misericórdia; 8 bem-aventurados os limpos de coração, porque eles verão
-        a Deus; 9 bem-aventurados os pacificadores, porque eles serão chamados
-        filhos de Deus; 10 bem-aventurados os que sofrem perseguição por causa
-        da justiça, porque deles é o Reino dos céus; 11 bem-aventurados sois vós
-        quando vos injuriarem, e perseguirem, e, mentindo, disserem todo o mal
-        contra vós, por minha causa. 12 Exultai e alegrai-vos, porque é grande o
-        vosso galardão nos céus; porque assim perseguiram os profetas que foram
-        antes de vós.
+      <q>
+        Glória a Deus nas maiores alturas, e paz na terra entre os homens a quem
+        ele ama
+      </q>
+      <br></br>
+      <cite>Lucas 2:14</cite>
+      <h2>Trindade</h2>
+      <p>Creio em Deus Pai... e no Filho... e no Espirito Santo</p>
+      <p>
+        O mistério da Santíssima Trindade, ao mesmo tempo que pode ser confuso,
+        estudar a respeito da Triunidade de Deus nos revela o quão magnifico e
+        grande é a magnetude do Deus todo poderoso. Jocobus de Voragine
+        (1228-1298) arcebispo de Gênova, conta uma história intrigante que
+        ilustra isso muito bem.
       </p>
+      <p>
+        O conto diz que Agostinho de Hipona, encontrou um menino enquanto
+        caminhava à beira-mar contemplando o mistério da santissima trindade. O
+        garoto corria de um lado para o outro, carregando a água do mar para uma
+        pequena cova que ele havia feito na areia. Quando Agostinho o perguntou
+        o motivo de estar fazendo issi, o garoto disse que iria despejar todo
+        oceano na pequena cova. Agostinho disse que isso seria impossivel, pois
+        a pequena cova é imcapaz de comportar a imenssidão do oceano. A criança
+        sabiamente concordou, porem adicionou que seria mais facil fazer o mar
+        servir na cova, do que tentar fazer a limitada e fragil sabedoria humana
+        comportar a eternidade e esplendor da Santíssima Trindade.
+      </p>
+      <p>
+        Trindade é um conceito teologico não expresso diretamente na Biblia,
+        porém é perfeitamente embasado e observado Bíblicamente, no Antigo
+        Testamento de forma implicita e sendo mais claramente revelado no Novo
+        Testamento. Deus sendo um só, em essencia havendo um só Deus que se
+        manifesta em três pessoas distintas porem não divididas. Pai Filho e
+        Espírito, O Grande Deus Triuno soberano sobre todas as coisas.
+      </p>
+      <section>
+        <h3>Antigo Testamento</h3>
+        <p>
+          O Senhor nos revela como sendo o unico Criador e Deus todo poderoso
+          logo no inicio de sua Sagrada escritura{" "}
+          <q>No princípio, Deus criou o céus e a terra</q>
+          <cite>(Gênesis 1.1)</cite> e mais a frente ainda na Torá, no{" "}
+          <i>Shema Yisrael</i>,é instituida a profissão de fé do povo de Deus,
+          que afirma a crença em um unico Ser divino{" "}
+          <q>Escute, Israel, o Senhor, nosso Deus, é o único Senhor</q>
+          <cite>(Deuteronomio 6.4)</cite>
+        </p>
+        <p>
+          De forma mais sutil percebemos algumas noções da pluraridade de Deus
+          dentro de sua unidade. No relato da Criação em Gênesis 1.1-3,
+          observamos a atuação de Deus, da Palavra e do Espírito. Posteriormente
+          em João 1 isso nós é confirmado, sendo Cristo a Palavra, o Verbo
+          encarnado meio pelo qual Deus cria todas as coisas{" "}
+          <q>
+            Todas as coisas foram feitas por ele e, sem ele, nada do que foi
+            feito se fez
+          </q>
+          <cite>(João 1.3)</cite>
+        </p>
+        <p>
+          O Messias no Antigo Testamento é descrito com traços e atribuições
+          divinas em algumas passagens (Salmos 16; 110; Isaias 9.6-8; Jeremias
+          23.5-6; Miqueias 5.2), uma que podemos destacar é o Salmo 110, de
+          Davi, que seria referido pelos apóstulos, declarando Jesus como o
+          Cristo e Senhor:{" "}
+          <q>
+            Porque Davi não subiu aos céus, mas ele mesmo declara: Disse o
+            Senhor ao meu Senhor: Assenta-te à minha direita, até que eu ponha
+            os teus inimigos por estrado dos teus pés. Esteja absolutamente
+            certa, pois, toda casa de Israel de que a este Jesus, que vós
+            crucificastes, Deus o fez Senhor e Cristo.
+          </q>
+          <cite>(Atos 2.34-36)</cite>. Assim podemos observar que desde o
+          princípio o Senhor vem se revelando como um único Deus, que possui em
+          si 3 pessoas distintas que são igualmente esse mesmo Deus.
+        </p>
+      </section>
+      <section>
+        <h3>Novo Testamento</h3>
+        <p>
+          No decorrer dos séculos, é chegado aquele quem Israel esperava, o
+          Cristo, o Deus que se faz homem, e as revelações sobre o Senhor
+          tornaram-se ainda mais claras e o que antes poderiamos inferir,
+          baseado na revelação até então, se confirma e se faz certo pelas
+          palavras do proprio Cristo e de seus apóstulos.
+        </p>
+        <p>
+          Varias passagens deixam plenamente explicito o Cristo como sendo um
+          com o Pai e o Espirito como Deus atuando na igreja como regenerador e
+          consolador, inclusive com as 3 pessoas de Deus interagindo e se
+          manifestando no mesmo espaço (Nascimento de Jesus; Batismo de Jesus; A
+          Grande Comissão; A Benção Apostólica; O amor do Deus triúno; Saudações
+          apostólicas), podemos citar uma dessas passagens como o bastismo de
+          Jesus:{" "}
+          <q>
+            Batizado Jesus, saiu logo da água, e eis que se lhe abriram os céus,
+            e viu o Espírito de Deus descendo como pomba, vindo sobre ele. E eis
+            uma voz dos céus, que dizia: Este é o meu Filho amado, em quem me
+            comprazo.
+          </q>
+          <cite>(Mateus 3.16-17)</cite> Jesus sendo Deus e homem, recebe o
+          Espirito Santo, e o Pai declara a aprovação do Filho, este é o quadro
+          e a Tridade compõe.
+        </p>
+        <p>
+          O Filho também aparece continuamente durante a revelação como sendo
+          adorado, assim como o Deus Pai, sendo possível reconhece-lo dessa
+          forma apenas por meio do Espírito Santo:{" "}
+          <q>
+            e ninguém pode dizer: "Jesus é Senhor", a não ser pelo Espírito
+            Santo.
+          </q>
+          <cite>(Corintios 12.3b)</cite>. A Trindade ainda permanece como um
+          ponto incompreendivel de forma total por nós, mas agora o Senhor se
+          faz revelado até onde devemos e consseguimos entender, ainda que como
+          um mistério.
+        </p>
+      </section>
+      <section>
+        <h3>O Drama de Redenção</h3>
+        <p>
+          As tres pessoas da Trindade são iguais, todos com os mesmos atributos,
+          todos são um unico Deus, porém é possivel fazer uma distinção de
+          papeis na obra de salvação, sem claro criar qualquer divisão em Deus,
+          apenas distinguir o papel assumido por cada pessoa da Trindade na
+          nossa salvação. Vamos analisar com base na carta de Paulo aos Efesios
+        </p>
+      </section>
     </>
   );
 }
