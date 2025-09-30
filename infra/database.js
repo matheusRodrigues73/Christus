a78f0a7708ff22a6847f93f79b014ecc69fe2c1a
Matheus Rodrigues <147061934+matheusRodrigues73@users.noreply.github.com>
2025-09-30 16:31:12 +0000

    fix self assigned ssl connections

--- infra/database.js
+++ infra/database.js
@@ -7,7 +7,7 @@ async function query(objectQuery) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
     port: process.env.POSTGRES_PORT,
-    ssl: process.env.NODE_ENV === "development" ? false : process.env.SSL_KEY,
+    ssl: process.env.POSTGRES_CA ? { ca: process.env.POSTGRES_CA } : false,
   });
   console.log({
     host: process.env.POSTGRES_HOST,
