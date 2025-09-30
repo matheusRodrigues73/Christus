5d27cab7df0d35a3a89aa69afa6088af7d5ab244
Matheus Rodrigues <147061934+matheusRodrigues73@users.noreply.github.com>
2025-09-30 16:08:17 +0000

    add self-assigned option of ssl connection with the database

--- infra/database.js
+++ infra/database.js
@@ -7,6 +7,7 @@ async function query(objectQuery) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
     port: process.env.POSTGRES_PORT,
+    ssl: process.env.NODE_ENV === "development" ? false : process.env.SSL_KEY,
   });
   console.log({
     host: process.env.POSTGRES_HOST,
