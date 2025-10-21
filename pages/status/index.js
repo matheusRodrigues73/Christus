2da4039d1a1b39dbfeab2813bdd1114eba8e234b
PrinzLiebe <129753212+PrinzLiebe@users.noreply.github.com>
2025-10-21 18:07:56 +0000

    style: add `space` between `{` and functions names, between components names and `/` too

--- pages/status/index.js
+++ pages/status/index.js
@@ -1,39 +1,43 @@
 import useSWR from "swr";
 
-async function fetchAPI(key){
+async function fetchAPI(key) {
   const response = await fetch(key);
   const responseBody = await response.json();
-  return responseBody
+  return responseBody;
 }
 
-export default function StatusPage(){
+export default function StatusPage() {
   return (
     <>
       <h1>Status</h1>
-      <UpdatedAt/>
-      <Dependencies/>
+      <UpdatedAt />
+      <Dependencies />
     </>
-  )
+  );
 }
 
-function UpdatedAt(){
-  const {isLoading, data} = useSWR("/api/v1/status", fetchAPI, {refreshInterval: 2000})
-  let updatedAtResponse = "Carregando..."
-  if(!isLoading && data){
-    updatedAtResponse = new Date(data.updated_at).toLocaleString("pt-BR")
+function UpdatedAt() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+  });
+  let updatedAtResponse = "Carregando...";
+  if (!isLoading && data) {
+    updatedAtResponse = new Date(data.updated_at).toLocaleString("pt-BR");
   }
-  return "Última atualização às: " + updatedAtResponse
+  return "Última atualização às: " + updatedAtResponse;
 }
 
-function Dependencies(){
-  const {isLoading, data} = useSWR("/api/v1/status", fetchAPI, {refreshInterval: 2000})
-  let versionResponse = "Carregando..."
-  let maxConnectionsResponse = "Carregando..."
-  let openedConnectionsResponse = "Carregando..."
-  if(!isLoading && data){
-    versionResponse = data.dependencies.database.version
-    maxConnectionsResponse = data.dependencies.database.max_connections
-    openedConnectionsResponse = data.dependencies.database.opened_connections
+function Dependencies() {
+  const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
+    refreshInterval: 2000,
+  });
+  let versionResponse = "Carregando...";
+  let maxConnectionsResponse = "Carregando...";
+  let openedConnectionsResponse = "Carregando...";
+  if (!isLoading && data) {
+    versionResponse = data.dependencies.database.version;
+    maxConnectionsResponse = data.dependencies.database.max_connections;
+    openedConnectionsResponse = data.dependencies.database.opened_connections;
   }
   return (
     <>
@@ -43,7 +47,7 @@ function Dependencies(){
         <li>Verção atual: {versionResponse}</li>
         <li>Maxímo de conexões suportadas: {maxConnectionsResponse}</li>
         <li>Conexões ativas: {openedConnectionsResponse}</li>
-      </ul>     
+      </ul>
     </>
-  )
+  );
 }
