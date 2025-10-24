169c5cd537f5bb882399046dc232676270af61cb
PrinzLiebe <129753212+PrinzLiebe@users.noreply.github.com>
2025-10-24 17:12:11 +0000

    chore: remove `node-pg-migrations` logs

--- models/migrator.js
+++ models/migrator.js
@@ -6,7 +6,7 @@ const defaultMigrationsConfig = {
   dryRun: true,
   dir: resolve("infra", "migrations"),
   direction: "up",
-  verbose: true,
+  log: () => {},
   migrationsTable: "pgmigrations",
 };
 
