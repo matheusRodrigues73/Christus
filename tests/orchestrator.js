add30a72170bf25c97f19dcc72f84f0a002c8ce6
Matheus Rodrigues <147061934+matheusRodrigues73@users.noreply.github.com>
2025-11-25 22:07:37 +0000

    feat: create `email.js` module

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -5,8 +5,11 @@ import migrator from "models/migrator";
 import session from "models/session";
 import user from "models/user";
 
+const emailHttpUrl = `http://${process.env.EMAIL_HTTP_HOST}:${process.env.EMAIL_HTTP_PORT}`;
+
 async function waitForAllServices() {
   await waitForWebServer();
+  await waitForEmailServer();
   async function waitForWebServer() {
     return retry(fetchStatusPage, {
       retries: 100,
@@ -15,6 +18,20 @@ async function waitForAllServices() {
 
     async function fetchStatusPage() {
       const response = await fetch("http://localhost:3000/api/v1/status");
+      if (response.status !== 200) {
+        throw new Error();
+      }
+    }
+  }
+
+  async function waitForEmailServer() {
+    return retry(fetchEmailPage, {
+      retries: 100,
+      maxTimeout: 1000,
+    });
+
+    async function fetchEmailPage() {
+      const response = await fetch(emailHttpUrl);
       if (response.status !== 200) {
         throw new Error();
       }
@@ -46,6 +63,23 @@ async function getUserByUsername(username) {
   return await user.findOneByUsername(username);
 }
 
+async function deleteAllEmails() {
+  await fetch(`${emailHttpUrl}/messages`, { method: "DELETE" });
+}
+
+async function getLastEmail() {
+  const emailListResponse = await fetch(`${emailHttpUrl}/messages`);
+  const emailListBody = await emailListResponse.json();
+  const lastEmail = emailListBody.pop();
+
+  const emailTextResponse = await fetch(
+    `${emailHttpUrl}/messages/${lastEmail.id}.plain`,
+  );
+  const emailTextBody = await emailTextResponse.text();
+  lastEmail.text = emailTextBody;
+  return lastEmail;
+}
+
 const orchestrator = {
   waitForAllServices,
   clearDatabase,
@@ -53,6 +87,8 @@ const orchestrator = {
   createUser,
   createSession,
   getUserByUsername,
+  deleteAllEmails,
+  getLastEmail,
 };
 
 export default orchestrator;
