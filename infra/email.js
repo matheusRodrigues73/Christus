29c325be857593fdcfce488524898d6d83567c80
Matheus Rodrigues <147061934+matheusRodrigues73@users.noreply.github.com>
2026-06-05 23:45:00 +0000

    refactor: improve error login in `email.send()`

--- infra/email.js
+++ infra/email.js
@@ -1,4 +1,5 @@
 import nodemailer from "nodemailer";
+import { ServiceError } from "./errors";
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_SMTP_HOST,
@@ -11,9 +12,18 @@ const transporter = nodemailer.createTransport({
 });
 
 async function send(mailOpitions) {
-  if (!mailOpitions.from)
-    mailOpitions.from = "Glorificat <contato@glorificat.com.br>";
-  await transporter.sendMail(mailOpitions);
+  try {
+    if (!mailOpitions.from)
+      mailOpitions.from = "Glorificat <contato@glorificat.com.br>";
+    await transporter.sendMail(mailOpitions);
+  } catch (error) {
+    throw new ServiceError({
+      message: "The email message couldn't be sent.",
+      action: "Check the mail service is active.",
+      cause: error,
+      context: mailOpitions,
+    });
+  }
 }
 
 const email = {
--- infra/errors.js
+++ infra/errors.js
@@ -126,3 +126,23 @@ export class ServerError extends Error {
     };
   }
 }
+
+export class ServiceError extends Error {
+  constructor({ cause, message, action, context }) {
+    super(message || "Service is offline", { cause });
+    this.name = "ServiceError";
+    this.action = action || "Check the connection with service";
+    this.statusCode = 503;
+    this.context = context;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+      context: this.context,
+    };
+  }
+}
