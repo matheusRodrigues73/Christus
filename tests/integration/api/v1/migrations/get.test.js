b685f0d141d910cae10fe888149e20f760dd206f
Matheus Rodrigues <147061934+matheusRodrigues73@users.noreply.github.com>
2026-08-12 12:38:33 +0000

    test: replace `Retrieving` to `Running` text in migrations tests descriptions

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -8,7 +8,7 @@ beforeAll(async () => {
 
 describe("GET api/v1/migrations", () => {
   describe("Anonymous User", () => {
-    test("Retrieving pending migrations", async () => {
+    test("Running pending migrations", async () => {
       const response = await fetch(`${webserver.origin}/api/v1/migrations`);
       expect(response.status).toBe(403);
 
@@ -23,7 +23,7 @@ describe("GET api/v1/migrations", () => {
     });
   });
   describe("Privileged User", () => {
-    test("Retrieving pending migrations", async () => {
+    test("Running pending migrations", async () => {
       const privilegedKey = process.env.PRIVILEGED_KEY;
       const response = await fetch(`${webserver.origin}/api/v1/migrations`, {
         headers: {
