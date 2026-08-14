f2fb0264a1d01103bd5faad4125cc23b1ba480a7
Matheus Rodrigues <147061934+matheusRodrigues73@users.noreply.github.com>
2026-08-14 18:35:11 +0000

    test: add a tolaration of 5 sec. to sessions timestamp

--- tests/integration/api/v1/sessions/post.test.js
+++ tests/integration/api/v1/sessions/post.test.js
@@ -119,10 +119,14 @@ describe("POST api/v1/sessions", () => {
       const expiresAt = new Date(responseBody.expires_at);
       const createdAt = new Date(responseBody.created_at);
 
-      expiresAt.setMilliseconds(0);
-      createdAt.setMilliseconds(0);
+      const sessionTimestamp = expiresAt - createdAt;
 
-      expect(expiresAt - createdAt).toBe(session.EXPIRATION_IN_MILLISECONDS);
+      expect(sessionTimestamp).toBeLessThanOrEqual(
+        session.EXPIRATION_IN_MILLISECONDS,
+      );
+      expect(expiresAt - createdAt).toBeGreaterThan(
+        session.EXPIRATION_IN_MILLISECONDS - 5000,
+      );
 
       const parsedSetCookie = setCookieParser(response, { map: true });
       expect(parsedSetCookie.session_id).toEqual({
