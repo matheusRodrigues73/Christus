cd4eae9187a31f3d64641d2cd9a98fc342df9319
Matheus Rodrigues <147061934+matheusRodrigues73@users.noreply.github.com>
2026-08-15 14:11:45 +0000

    build: configure `eslint` to use `css` with `tailwind`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,57 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import js from "@eslint/js";
+import globals from "globals";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import pluginJest from "eslint-plugin-jest";
+import prettier from "eslint-config-prettier/flat";
+import json from "@eslint/json";
+import markdown from "@eslint/markdown";
+import css from "@eslint/css";
+import { tailwind3 } from "tailwind-csstree";
 
 export default defineConfig([
-  { files: ["**/*.js"], plugins: { js }, extends: ["js/recommended"] },
   {
-    files: ["**/*.test.js"],
-    plugins: { pluginJest },
-    extends: ["pluginJest/recommended"],
+    files: ["**/*.js"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  {
+    files: ["tests/**/*.test.js"],
+    ...pluginJest.configs["flat/recommended"],
   },
   ...nextVitals,
+  prettier,
+  {
+    files: ["**/*.json"],
+    plugins: { json },
+    language: "json/json",
+    extends: ["json/recommended"],
+    ignores: ["package-lock.json"],
+  },
+  {
+    files: ["**/*.jsonc"],
+    plugins: { json },
+    language: "json/jsonc",
+    extends: ["json/recommended"],
+  },
+  {
+    files: ["**/*.json5"],
+    plugins: { json },
+    language: "json/json5",
+    extends: ["json/recommended"],
+  },
+  {
+    files: ["**/*md"],
+    plugins: { markdown },
+    language: "markdown/gfm",
+    extends: ["markdown/recommended"],
+  },
+  {
+    files: ["**/*.css"],
+    plugins: { css },
+    language: "css/css",
+    languageOptions: { customSyntax: tailwind3 },
+    extends: ["css/recommended"],
+  },
 ]);
